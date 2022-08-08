cc85a3cbd2f6c6f1dbdae84a5e3b9d869ebdd293
Bocius <mama@gmail.com>
2022-08-08 11:06:14 +0300

    Some visual fixes

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 //Isnykstantis div
 function disappearingDiv(x) {
-    document.getElementsByClassName('disappear')[x].classList.toggle('disappearing-div')
+    document.getElementsByClassName('disappear')[x].classList.toggle('disappearing-div');
+    document.getElementsByClassName('disappear')[x].classList.remove('height');
+    document.getElementsByClassName('disappear')[x].classList.remove('width');
+    document.getElementsByClassName('disappear')[x].classList.remove('center');
+    document.getElementsByClassName('disappear')[x].classList.remove('position');
 }
 //div aukstis
 function height(x) {
-    document.getElementsByClassName('disappear')[x].classList.toggle('height')
+    document.getElementsByClassName('disappear')[x].classList.toggle('height');
 }
 //div plotis
 function width(x) {
-    document.getElementsByClassName('disappear')[x].classList.toggle('width')
+    document.getElementsByClassName('disappear')[x].classList.toggle('width');
 }
 //div centravimas
 function center(x) {
-    document.getElementsByClassName('disappear')[x].classList.toggle('center')
+    document.getElementsByClassName('disappear')[x].classList.toggle('center');
 }
 //div pozicija
 function position(x) {
-    document.getElementsByClassName('disappear')[x].classList.toggle('position')
+    document.getElementsByClassName('disappear')[x].classList.toggle('position');
 }
+
+
 //spalvos
 function rand() {
     return Math.floor(Math.random() * 256);
@@ -36,10 +42,16 @@ const box3 = document.getElementsByClassName('bb3')[0];
 const box4 = document.getElementsByClassName('bb4')[0];
 
 function recolor(event, obj) {
-    box1.style.backgroundColor = randomColor();
-    box2.style.backgroundColor = randomColor();
-    box3.style.backgroundColor = randomColor();
-    box4.style.backgroundColor = randomColor();
+
+    const boxa = box1.style.backgroundColor = randomColor();
+    const boxb = box2.style.backgroundColor = randomColor();
+    const boxc = box3.style.backgroundColor = randomColor();
+    const boxd = box4.style.backgroundColor = randomColor();
+    box1.innerHTML = boxa;
+    box2.innerHTML = boxb;
+    box3.innerHTML = boxc;
+    box4.innerHTML = boxd;
+
 }
 /// numeracijos skaiciuotuvas
 let i = 0;
@@ -63,17 +75,21 @@ $(document).ready(function () {
         let right = $('#nav').css('right');
         if (right == '0px') {
             let nav = $('#nav');
+            $('#burger').removeClass('transparent');
+            $('#burgerX').addClass('transparent');
             nav.animate({
                 right: '-110%',
             }, 1000);
         }
         else {
+            $('#burger').addClass('transparent');
+            $('#burgerX').removeClass('transparent');
             displayNav();
         }
     })
 // pakeisti spalvas
-    $('.blackBox').click(function () {
-        recolor(event, this)
+    $('.blackBox').dblclick(function () {
+        recolor(event, this);
     })
 // lenteles kodas
     $('#submit').click(function () {
@@ -87,13 +103,12 @@ $(document).ready(function () {
         $('#name').val('');
         $('#lastName').val('');
     })
+    //Isvalyti lentele
     $('#empty').click(function () {
         $('#age').val('');
         $('#name').val('');
         $('#lastName').val('');
         $('#tbody').empty();
         $('#table').css('display', 'none')
-
-
     })
 })
